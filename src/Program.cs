1689d9801d55226ec4c77a4d5dfe27095265d142
Akobir <akobirabduvahobov@gmail.com>
2024-11-07 11:59:37 +0500

    Code added

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace src
+﻿namespace src;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        var ones = number % 10;
+        var tens = number / 10 % 10;
+        var hundreds = number / 100 % 10;
+        var thousands = number / 1000 % 10;
+
+        var result = ones + tens + hundreds + thousands;
+
+        Console.WriteLine(result);
     }
 }
